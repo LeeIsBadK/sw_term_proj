adda3e7244cfed1eb1179f45076432be6bfa070e
sorapat-s <s.samsombudsagoon@gmail.com>
2024-11-10 00:02:36 +0700

    Edit home page

--- app/tabs/HomeScreen.tsx
+++ app/tabs/HomeScreen.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
+  const [number, setNumber] = useState(0);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image source={require('../../assets/images/icon.png')} style={{ width: 200, height: 200 }} />
+    <View style={styles.container}>
+      <Text style={styles.header}> YOUR TAX THIS YEAR </Text>
+      <Text style={styles.yearText}> {currentYear} </Text>
+      <View style={styles.box}>
+        <Text style={styles.numberText}> {number} </Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: 'lightblue',
+    padding: 20,
+    borderRadius: 10,
+  },
+  numberText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  yearText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 10,
+    paddingBottom: 20,
+  },
+});
 
 export default HomeScreen;
