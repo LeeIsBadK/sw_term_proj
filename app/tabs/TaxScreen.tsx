291f87d3c6f612f20142dffb93b64aa11faedeea
sorapat-s <s.samsombudsagoon@gmail.com>
2024-11-09 23:36:09 +0700

    clean up screen files, change login to index for default page

--- app/tabs/TaxScreen.tsx
+++ app/tabs/TaxScreen.tsx
@@ -4,8 +4,7 @@ import { View, Image } from 'react-native';
 const TabScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image source={require('../../assets/images/icon.png')} style={{ width: 200, height: 200  
- }} />
+      <Image source={require('../../assets/images/icon.png')} style={{ width: 200, height: 200 }} />
     </View>
   );
 };
